47ae222d9570f318ba51fb45fbbb9d830d3ebe05
Ismail <solist.cilginlik.0h@icloud.com>
2024-12-20 15:07:56 +0300

    project getting entire rewrite

--- predict.py
+++ predict.py
@@ -1,25 +1,20 @@
-test_image_path = input("PGM file path: ")
-
-import tensorflow as tf
 import numpy as np
-from tensorflow.keras.preprocessing import image
-import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from PIL import Image
 
-model_path = 'breast_cancer.keras'
+model = load_model('breast_cancer_model.keras')
 
-model = tf.keras.models.load_model(model_path)
+def predict_image(image_path):
+    img = Image.open(image_path).convert('RGB')
+    img = img.resize((150, 150))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    return 'Cancer' if prediction[0] > 0.5 else 'Non-Cancer'
 
-img = image.load_img(test_image_path, target_size=(224, 224), color_mode='rgb')
-img_array = image.img_to_array(img) / 255.0
-img_array = np.expand_dims(img_array, axis=0)
-
-prediction = model.predict(img_array)[0][0]
-
-print(f"Prediction: {prediction:.4f}")
-
-if prediction > 0.7:
-    print("Infected")
-else:
-    print("Normal")
-
-img.show()
+while True:
+    path = input("Dosya konumunu giriniz (lütfen🫸🫷): ")
+    if path=="x":
+        break
+    else:
+        print(predict_image(path))
--- train_model.py
+++ train_model.py
@@ -1,61 +1,51 @@
 import os
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
 
-infected_path = 'imgs/infected'
-normal_path = 'imgs/NORM'
+cancer_dir = "/mnt/c/Users/MONSTER/Documents/BreastCancerPrediction/datset/augmented/Cancer"
+non_cancer_dir = "/mnt/c/Users/MONSTER/Documents/BreastCancerPrediction/datset/augmented/Non-Cancer"
 
-def load_data():
+def load_images(directory, label):
     images = []
     labels = []
-
-    for label, folder in enumerate([infected_path, normal_path]):
-        for file in os.listdir(folder):
-            img_path = os.path.join(folder, file)
-            img = image.load_img(img_path, target_size=(300, 300))
-            img_array = image.img_to_array(img) / 255.0
-            images.append(img_array)
+    for filename in os.listdir(directory):
+        img_path = os.path.join(directory, filename)
+        try:
+            img = Image.open(img_path).convert('RGB')
+            img = img.resize((150, 150))
+            images.append(np.array(img))
             labels.append(label)
-
+        except Exception as e:
+            pass
     return np.array(images), np.array(labels)
 
-images, labels = load_data()
+cancer_images, cancer_labels = load_images(cancer_dir, 1)
+non_cancer_images, non_cancer_labels = load_images(non_cancer_dir, 0)
 
-x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+X = np.concatenate((cancer_images, non_cancer_images), axis=0)
+y = np.concatenate((cancer_labels, non_cancer_labels), axis=0)
 
-base_model = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(300, 300, 3))
+X = X / 255.0
 
-for layer in base_model.layers:
-    layer.trainable = True
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = Sequential([
-    base_model,
-    BatchNormalization(),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-    Dense(256, activation='relu'),
-    Dropout(0.5),
-    Dense(1, activation='sigmoid')
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(
-    x_train, y_train,
-    validation_data=(x_test, y_test),
-    epochs=50,
-    batch_size=16
-)
+model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), batch_size=32)
 
-results = model.evaluate(x_test, y_test, verbose=0)
-test_loss, test_acc = results[0], results[1]
-print(f'Test Loss: {test_loss}, Test Accuracy: {test_acc}')
-
-model.save('breast_cancer.keras')
+model.save('breast_cancer_model.keras')
